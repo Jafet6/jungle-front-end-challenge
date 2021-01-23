0772162f766124b21fb6514119832ca654e2b51c
Jafet Henrique <jafetguerra6@gmail.com>
2021-01-23 18:33:32 -0300

    standard section done

---
+++ jungle-dev-front-end-challenge/src/components/imageParagraphComponent/structures/frameWorkStructure.js
@@ -0,0 +1,17 @@
+const frameWorkStructure = {
+  title: {
+    className: 'standard-section-h2',
+    text: 'A framework built for the long term'
+  },
+  paragraph: {
+    className: 'standard-section-p',
+    text: 'Childcare is for the long term. And you need a framework you can count on that gives your share long term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly defined process, transparency over money and equality of participation.'
+  },
+  link: {
+    className: 'share-nany-cost-link',
+    text: 'Read how Hapu’s tribal background defines our app ',
+    href: '8',
+  },
+}
+
+export default frameWorkStructure;
